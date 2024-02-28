5a0eda9b30925012956ab51275d7f1ffadf6d405
ndthang <ndthang@istt.com.vn>
2024-02-28 17:01:09 +0700

    q

--- inbound-traffic/src/main/java/com/istt/inboundtraffic/service/Constants.java
+++ inbound-traffic/src/main/java/com/istt/inboundtraffic/service/Constants.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public final class Constants {
 
-	public static final String host = "192.168.13.50";
+	public static final String host = "10.0.40.153";
 
 	public static final int port = 5060;
 
